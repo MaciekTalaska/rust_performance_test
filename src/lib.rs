8debf3c2cb034388058b916b1826d88f78115f3e
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-14 18:17:19 +0200

    simple test added

--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,15 @@ pub fn compute_imperative(count: u8) -> u32 {
     value
 }
 
-//#[cfg(test)]
-//mod dices_test {
-//    fn max_by_dice_num(dice_num: u8) -> u32 {
-//        (6 as u32).pow(dice_num as u32) - 1
-//    }
-//
-//    #[test]
-//    fn roll_dices_5_dices_should_not_exceed_7775_i() {
-//        for _i in { 0..1000 } {
-//            let result = self::super::roll_dices_i(5);
-//            assert!(result <= max_by_dice_num(5));
-//        }
-//    }
-//
-//    #[test]
-//    fn roll_dices_5_dices_should_not_exceed_7775_f() {
-//        for _i in { 0..1000 } {
-//            let result = self::super::roll_dices_f(5);
-//            assert!(result <= max_by_dice_num(5));
-//        }
-//    }
-//}
+#[cfg(test)]
+mod computational_test {
+    #[test]
+    fn compare() {
+        for _i in { 0..100 } {
+            let functional = super::compute_functional(5);
+            let imperative = super::compute_imperative(5);
+            assert_eq!(functional, imperative);
+        }
+    }
+
+}
