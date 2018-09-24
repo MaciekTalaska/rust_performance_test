81eb2447f905b6a8418c4e35dfbb8c144cc3c446
MaciekTalaska <maciek.talaska@gmail.com>
2018-09-24 15:06:44 +0200

    don't use namespaces if not needed

--- benches/vectors.rs
+++ benches/vectors.rs
@@ -4,19 +4,20 @@ extern crate performance_test;
 
 use bencher::Bencher;
 use bencher::black_box;
+use performance_test::*;
 
 const VECTOR_SIZE: u32 = 4;
 
 fn imperative(bench: &mut Bencher) {
-    bench.iter( || black_box(performance_test::build_vector_imperative(VECTOR_SIZE)));
+    bench.iter( || black_box(build_vector_imperative(VECTOR_SIZE)));
 }
 
 fn functional(bench: &mut Bencher) {
-    bench.iter( || black_box(performance_test::build_vector_functional(VECTOR_SIZE)));
+    bench.iter( || black_box(build_vector_functional(VECTOR_SIZE)));
 }
 
 fn recursive(bench: &mut Bencher) {
-    bench.iter( || black_box(performance_test::build_vector_recursive(VECTOR_SIZE)));
+    bench.iter( || black_box(build_vector_recursive(VECTOR_SIZE)));
 }
 
 benchmark_group!(build_vector, imperative, functional, recursive);
